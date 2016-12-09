a40f5f397f8d891bf793eab4a152b31b730b845b
Oula Kuuva <oula.kuuva@synopsys.com>
2016-12-09 19:59:29 +0200

    Create UBIRMPClass
    Moved all the functionality into class methods, as I'm going to
    completely overhaul the key press checking and having the main window as
    a object variable is going to handy then.

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 import sys
 import locale
-from curses import wrapper
+import curses
+
 from rmp import RMP
 from time import sleep
 from serial import SerialException
@@ -15,52 +16,62 @@ right = ["KEY_RIGHT", "d"]
 left = ["KEY_LEFT", "a"]
 
 
-def main(stdscr, port="/dev/ttyUSB0"):
-    stdscr.clear()
-    try:
-        rmp = RMP(port, debug=False)
-    except SerialException:
-        stdscr.addstr(0, 0, "{:50}".format("RMP init failed, exiting..."))
-        sys.exit(1)
-    old_command = ""
-    while True:
-        output = ""
-        command = stdscr.getkey()
-        if command != old_command:
-            sleep(0.25)
-        if command == "KEY_BACKSPACE":
-            break
-        elif command == "KEY_F(12)":
-            restart_browser()
-            output = "Restart browser"
-        elif command == " ":
-            click_screen()
-            output = "Click screen"
-        elif command in forward:
-            rmp.forward()
-            output = "FORWARD"
-        elif command in backward:
-            rmp.backward()
-            output = "BACKWARD"
-        elif command in left:
-            rmp.left()
-            output = "LEFT"
-        elif command in right:
-            rmp.right()
-            output = "RIGHT"
-        stdscr.addstr(0, 0, "{:50}".format(output))
-        old_command = command
+class UBIRMPController:
+    def __init__(self, main_window, port):
+        curses.curs_set(0)
+        self.main_window = main_window
+        self.main_window.clear()
+        try:
+            self.rmp = RMP(port, debug=False)
+        except SerialException:
+            self.main_window.addstr(0, 0, "{:50}".format("RMP init failed, exiting..."))
+            sys.exit(1)
+
+    def main_loop(self):
+        old_command = ""
+        while True:
+            output = ""
+            command = self.main_window.getkey()
+            if command != old_command:
+                sleep(0.25)
+            if command == "KEY_BACKSPACE":
+                break
+            elif command == "KEY_F(12)":
+                self.restart_browser()
+                output = "Restart browser"
+            elif command == " ":
+                self.click_screen()
+                output = "Click screen"
+            elif command in forward:
+                self.rmp.forward()
+                output = "FORWARD"
+            elif command in backward:
+                self.rmp.backward()
+                output = "BACKWARD"
+            elif command in left:
+                self.rmp.left()
+                output = "LEFT"
+            elif command in right:
+                self.rmp.right()
+                output = "RIGHT"
+            self.main_window.addstr(0, 0, "{:50}".format(output))
+            old_command = command
+
+    @staticmethod
+    def click_screen():
+        click = ["xdotool", "mousemove", "960", "920", "click", "1"]
+        call(click)
+
+    @staticmethod
+    def restart_browser():
+        call(["pkill", "chromium-browse"])
+        sleep(1)
+        call(["/home/pi/ubicomp.sh"])
 
 
-def click_screen():
-    click = ["xdotool", "mousemove", "960", "920", "click", "1"]
-    call(click)
-
-
-def restart_browser():
-    call(["pkill", "chromium-browse"])
-    sleep(1)
-    call(["/home/pi/ubicomp.sh"])
+def main(stdscr, port):
+    controller = UBIRMPController(stdscr, port)
+    controller.main_loop()
 
 
 if __name__ == "__main__":
@@ -68,4 +79,4 @@ if __name__ == "__main__":
         port = sys.argv[1]
     except IndexError:
         port = "/dev/ttyUSB0"
-    wrapper(main, port)
+    curses.wrapper(main, port)
